93e8382c545365a05f377a769b53f7b7141f7e4a
rnsheehan <robertnsheehan@gmail.com>
2016-12-19 23:05:42 +0000

    Added a function to do another commit and push to remote.

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -2,9 +2,30 @@
 #include <cstdlib>
 #include <iomanip>
 
+int factorial(int num); 
+
 int main()
 {
 	std::cout<<"Hello World\n"; 
 
 	return 0; 
+}
+
+int factorial(int num)
+{
+	// Compute the factorial of a given number
+	// limited to 30!
+
+	if(num<0){
+		return 0; 
+	}
+	else if (num == 0 || num == 1){
+		return 1; 
+	}
+	else if(num == 2){
+		return 2; 
+	}
+	else{
+		return num * factorial(num-1); 
+	}
 }
